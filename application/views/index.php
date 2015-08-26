b08ee497f8d8a43f2269c3c40faf910d08d0382f
John-Go <john@akaon.com>
2015-08-26 17:13:07 +0900

    musio animation

--- application/views/index.php
+++ application/views/index.php
@@ -1,16 +1,17 @@
 	<div id="video-container" class="hidden-xs hidden-sm">
-		<video muted="muted" preload="auto" loop="loop" autoplay="true" type="video/mp4" poster="./images/blur.jpg">
+		<video muted="muted" preload="auto" loop="loop" autoplay="true" type="video/mp4" poster="./public/images/background_musio.jpg">
+		    <!-- <source src="./public/images/musio_ani.mp4" type="video/mp4"> -->
+		    <source src="./public/images/musio_ani.mp4">
+		    <source src="./public/images/musio_ani_2.mp4" type="video/mp4">
 		    <source src="./public/images/test.mp4" type="video/mp4">
-		    <source src="movie.webm" type="video/webm">Sorry, your browser does not support HTML5 video.
+		    <!-- <source src="movie.webm" type="video/webm">Sorry, your browser does not support HTML5 video. -->
+		    
+		    <!-- <source src="https://igcdn-videos-b-19-a.akamaihd.net/hphotos-ak-xaf1/t50.2886-16/11912262_471872732983719_24618549_n.mp4"> -->
 		</video>
 		<div class="col-md-12 text-center" style="margin-top:50%;"> 
 			<button class="btn btn-default btn-lg">Pre-order Now</button>
 		</div>
 	</div>
-
 	
-	<div class="container">
-		
+	<div class="container">		
 	</div> <!-- /container -->
-	
-		
